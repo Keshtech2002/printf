5c778b4c61c33bad30be776915914ef561a774d2
Keshtech2002 <keshtech2002@gmail.com>
2022-05-20 02:47:19 -0700

    start

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - function that prints based on format specifier
+ * @format: takes in format specifier
+ * Return: the number of characters printed 
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
