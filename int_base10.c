e0591af6a84ab91b82be41d4f926369a7b990bfa
itsmmagain <mmtsalman009@gmail.com>
2022-05-23 23:21:47 -0700

    fix %i 4

--- int_base10.c
+++ int_base10.c
@@ -10,33 +10,33 @@
 int _print_i(va_list i)
 {
 	int a[10];
-	int i, x, y, add, count;
+	int k, x, y, add, count;
 
 	y = va_arg(i, int);
 	count = 0;
 	x = 1000000000;
 	a[0] = y / x;
 
-	for (i = 1; i < 10; i++)
+	for (k = 1; k < 10; k++)
 	{
 		x = x / 10;
-		a[i] = (y / x) % 10;
+		a[k] = (y / x) % 10;
 	}
 
 	if (y < 0)
 	{
 		_putchar('-');
 		count = count + 1;
-		for (i = 0; i < 10; i++)
-			a[i] *= -1;
+		for (k = 0; k < 10; k++)
+			a[k] *= -1;
 	}
 
-	for (i = 0, add = 0; i < 10; i++)
+	for (k = 0, add = 0; k < 10; k++)
 	{
-		add = add + a[i];
-		if (add != 0 || i == 9)
+		add = add + a[k];
+		if (add != 0 || k == 9)
 		{
-			_putchar('0' + a[i]);
+			_putchar('0' + a[k]);
 			count = count + 1;
 		}
 	}
